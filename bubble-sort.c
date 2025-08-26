4108dcf83d6c818f688ab611385fa9a438803cc0
tadeeb <tadeeb@Thesis-work.myguest.virtualbox.org>
2025-08-26 13:38:22 +0300

    Added Linear sort and Bubble sort without pointer usage

---
+++ bubble-sort.c
@@ -0,0 +1,28 @@
+/* Bubble Sort*/
+#include<stdio.h>
+int main()
+{
+int length, in, out,count, buffer;
+int x[5] = {50,70,30,8,12};
+length = 5;
+count=length;
+for(int i=0;i<length-1;i++)
+{
+	for(out=0,in=out+1; in<count ;out++,in++)
+	{
+		buffer=x[in];
+		if(x[out]>x[in]) 
+		{
+			x[in]=x[out];
+			x[out]=buffer;
+		}
+	}
+count--;
+}
+
+for(int i=0;i<length;i++)
+{
+printf("%d\n",x[i]);
+}
+return 0;
+}
---
+++ linear_sort.c
@@ -0,0 +1,26 @@
+/* Linear Sort*/
+#include<stdio.h>
+int main()
+{
+int length, buffer;
+int x[5] = {50,30,70,29,15};
+length=5;
+for(int out=0; out<length-1; out++)
+{
+	for(int in=out+1;  in<length; in++)
+	{
+		if(x[in] < x[out])
+		{
+			buffer=x[out];
+			x[out] = x[in];
+			x[in] = buffer;
+		}
+	}
+}
+
+for(int i=0;i<length;i++)
+{
+printf("%d\n", x[i]);
+}
+return 0;
+}
