a1e39cec39bacc7bf0ed1171fa6f68865104f206
smTadeeb <70131688+smTadeeb@users.noreply.github.com>
2025-08-27 11:23:07 +0300

    Quick sort also added to array_sort.c

--- all_sorts.c
+++ all_sorts.c
@@ -4,10 +4,12 @@ void see_array(int *,int);
 void bubble_sort(int *,int);
 void selection_sort(int *,int);
 void insertion_sort(int *,int);
+void quick_sort(int *,int,int);
+int find_partition_point(int *, int , int );
+
 
 int main()
 {
-
 int x[6];
 
 int x_copy[6];
@@ -25,7 +27,6 @@ for(int i=0;i<length;i++)
 x_copy[i] = x[i];
 }
 
-
 while(1)
 {
 printf("\n1. Bring Array to Unsorted State");
@@ -33,8 +34,9 @@ printf("\n2. Linear Sort using pointers");
 printf("\n3. Bubble Sort using pointers");
 printf("\n4. Selection Sort using pointers");
 printf("\n5. Insertion Sort using pointers");
-printf("\n6. See Array");
-printf("\n7. Exit\n\n");
+printf("\n6. Quick Sort using pointers");
+printf("\n7. See Array");
+printf("\n8. Exit\n\n");
 
 printf("Enter your choice: ");
 scanf("%d", &ch);
@@ -47,29 +49,34 @@ x[i] = x_copy[i];
 }
 else if(ch==2)
 {
-printf("\n____Array got Linear Sorted___\n");
+printf("\n____Array got Linear Sorted___\n\n");
 linear_sort(x,length);
 }
 else if(ch==3)
 {
-printf("\n____Array got Bubble Sorted___\n");
+printf("\n____Array got Bubble Sorted___\n\n");
 bubble_sort(x,length);
 }
 else if(ch==4)
 {
-printf("\n____Array got Selection Sorted___\n");
+printf("\n____Array got Selection Sorted___\n\n");
 selection_sort(x,length);
 }
 else if(ch==5)
 {
-printf("\n____Array got Insertion Sorted___\n");
+printf("\n____Array got Insertion Sorted___\n\n");
 insertion_sort(x,length);
 }
 else if(ch==6)
 {
-see_array(x,length);
+printf("\n____Array got Quick Sorted___\n\n");
+quick_sort(x,0,length-1);
 }
 else if(ch==7)
+{
+see_array(x,length);
+}
+else if(ch==8)
 {
 break;
 }
@@ -155,7 +162,6 @@ ba++;
 }
 }
 
-
 void insertion_sort(int *ba, int length)
 {
 int *oep, *iep, *dimag, *bac;
@@ -184,13 +190,12 @@ iep = ba-1;
 	}
 }
 
-
-/* //Above Insertion sort just implemented with for loop
+/* //Insertion Sort with for loop 
 void insertion_sort(int *ba, int length)
 {
 int *oep, *iep, *dimag, *bac;
 int dimag_save;
-	for(dimag = ba+1, oep = ba+length, iep = ba-1;  dimag!=oep; dimag++)
+	for(dimag = ba+1, oep = ba+length, iep = ba-1;  dimag!=oep;  dimag++)
 	{
 		for(dimag_save=*dimag, bac = dimag-1; bac != iep; *(bac+1)=*bac, bac--)
 		{
@@ -200,7 +205,42 @@ int dimag_save;
 				break;
 			}
 		}
-		if(bac == iep)  *(bac+1)= dimag_save;
+	if(bac == iep)  *(bac+1)= dimag_save;
 	}
 }
 */
+
+int find_partition_point(int *ba, int lb, int ub)
+{
+int e,f,g;
+e=lb;
+f=ub;
+while(1)
+{
+while(ba[e]<=ba[lb] && e<ub) e++; 
+while(ba[f]>ba[lb]) f--; 
+if(e<f)
+{
+g=ba[e];
+ba[e]=ba[f];
+ba[f]=g;
+}
+else
+{
+g=ba[lb];
+ba[lb]=ba[f];
+ba[f]=g;
+break;
+}
+}
+return f;
+}
+
+void quick_sort(int *ba, int lb, int ub)
+{
+int partition_point;
+if(lb>=ub) return;
+partition_point = find_partition_point(ba,lb,ub);
+quick_sort(ba,lb,partition_point-1);
+quick_sort(ba,partition_point+1,ub);
+}
