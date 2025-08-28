808e7c12d236ca8c74a40f62e9ca6d8d32bcc5ad
smTadeeb <70131688+smTadeeb@users.noreply.github.com>
2025-08-28 19:28:18 +0300

    Now the all_trim.c code is working properly

--- all_trim.c
+++ all_trim.c
@@ -10,18 +10,15 @@ char str[500];
 
 printf("\nEnter the string: ");
 scanf("%[^\n]s", str);
-
 printf("Length of original: %u\n", strlen(str));
-left_trim(str);
-
-printf("Length after left trim: %u\n", strlen(str));
 
 right_trim(str);
-
 printf("Length after right trim: %u\n", strlen(str));
 
-mid_trim(str);
+left_trim(str);
+printf("Length after left trim: %u\n", strlen(str));
 
+mid_trim(str);
 printf("Length of final: %u\n", strlen(str));
 
 printf("\nThe string is: %s \n\n", str);
@@ -49,35 +46,38 @@ while(*p== ' ') p--;
 *(p+1) = '\0';  
 }
 
-// This is not completely working right now.
 void mid_trim(char *p)
 {
 int count=0;
 int runner = count+1;
-char *q,*r;
+char *q,*r,*marked_point;
 q=p;
 
 while(1)
 {
-while((*q != ' ' || count == 0) && *q!='\0') 
+while((*q != ' ' || count == 0) && *q!='\0') //this code is creative as it continues the loop if there is only a single space
+                                              // but it doesnt not continues its work when there comes second single space and hence it breaks 
+                                              // because when the second single space word enters this becomes false->(*q != ' ' || count == 0)
 {
 if(*q==' ') count++;
+else count=0; // so i reset count to zero when there is no space so that(*q != ' ' || count == 0) will stay true until there are multispaces
 q++;
 }
-r=q;
+marked_point=r=q;
+
 while(*r == ' ') r++;
 
-while((*r != ' ' || *r != '\0') && *r!='\0')
+while(*r != ' ' && *r!='\0')   // previously while((*r != ' ' || *r != '\0') && *r!='\0') 
 {
-*q=*r;
-*r = ' ';
+*q=*r; 
+*r = ' ';  //(i think its not neccesary)
 q++;
 r++;
 }
-if(*r=='\0') *q = '\0';
-
-if(*q=='\0') break;
-q=p;
+if(*r=='\0') *q = '\0'; 
+if(*q=='\0') break; 
+else *q=' ';  
+q=marked_point;  // previously q=p;
 r=NULL;
 count=0;
 }
