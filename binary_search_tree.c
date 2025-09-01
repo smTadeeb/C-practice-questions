798acf1c6362bb32412d835df367f2d0f7b0a8b6
smTadeeb <70131688+smTadeeb@users.noreply.github.com>
2025-09-02 00:25:38 +0300

    Building BInary search tree code

--- binary_search_tree.c
+++ binary_search_tree.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<stdlib.h>
 
 struct bst_node
 {
@@ -6,23 +7,100 @@ int num;
 struct bst_node *left, *right;
 };
 
+struct bst_node *bst_root;
+
 
 void add_to_bst(int num)
 {
-struct bst_node *t;
-t=(struct bst_node *)malloc(sizeof(struct bst_node));
+struct bst_node *t, *j;
+t=bst_root;
+while(t!=NULL)
+{
+if(t->num == num)
+{
+printf("\n\nThis number already exist, Kindly enter a unique number\n\n");
+return;
+}
+if(num < t->num)
+{
+t=t->left;
+}
+if(num > t->num)
+{
+t=t->right;
+}
+}
 
-
-
+t= (struct bst_node*)malloc(sizeof(struct bst_node));
 t->num=num;
 t->left=NULL;
 t->right=NULL;
+
+j=bst_root;
+while(j)
+{
+
+	if(t->num < j->num)
+	{
+		if(j->left)
+		{
+		j=j->left;
+		}
+		else
+		{
+		j->left=t;
+		}
+	}
+
+	if(t->num > j->num)
+	{
+		if(j->right)
+		{
+		j=j->right;
+		}
+		else
+		{
+		j->right = t;
+		}
+	}
+}
+printf("\nNumber %d added Successfully\n\n", num);
 }
 
 
+void remove_from_bst(int num)
+{
+}
+
+void __bst_inorder_traversal(struct bst_node *root)
+{
+struct bst_node *t;
+t=root;
+if(t == NULL) return;
+__bst_inorder_traversal(t->left);
+printf("%d ", t->num);
+__bst_inorder_traversal(t->right);
+}
+
+void bst_inorder_traversal()
+{
+__bst_inorder_traversal(bst_root);
+}
+
+void bst_preorder_traversal()
+{
+//__bst_preorder_traversal(bst_root);
+}
+
+void bst_postorder_traversal()
+{
+//__bst_postorder_traversal(bst_root);
+}
+
 int main()
 {
-int ch num;
+int ch , num;
+bst_root = NULL;
 while(1)
 {
 printf("\n1. Add Node/data");
