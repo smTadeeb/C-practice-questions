481666887b7df3da60fc16585dd85991b28cc0b9
smTadeeb <70131688+smTadeeb@users.noreply.github.com>
2025-09-27 11:36:29 +0300

    stl::vector capacity methods practice

--- cpp-questions/vector.cpp
+++ cpp-questions/vector.cpp
@@ -11,7 +11,7 @@ cout<<"size of v1 objects vector is: "<<v1.size()<<endl;
 cout<<"capacity of v1 objects vector is: "<<v1.capacity()<<endl;
 cout<<"max_size of v1 objects vector is: "<<v1.max_size()<<endl;
 
-v1={1,2,3}; // v1.=({1,2,3}) operator overloading will run here. and accordingly size of vector array for v1 object will be allocated in heap region.
+if(v1.empty()) v1={1,2,3}; // v1.=({1,2,3}) operator overloading will run here. and accordingly size of vector array for v1 object will be allocated in heap region.
 
 cout<<endl<<"The elements in v1 objects vector(array) will be: ";
 for(int x : v1)
